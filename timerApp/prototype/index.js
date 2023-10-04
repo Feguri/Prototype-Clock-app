1120328255183c892008617074658bf90b7b1d3b
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2023-10-04 17:22:36 -0400

    commit thing

--- timerApp/prototype/index.js
+++ timerApp/prototype/index.js
@@ -37,15 +37,45 @@ for (let btn of document.getElementsByTagName('svg')) {
 
 // pause/play button
 let currentState = 'play';
+function countdownChange(change) {
+    let thingsToChange = document.getElementsByClassName('a');
+    if(change == 'show'){
+        for (let obj of thingsToChange) {
+            obj.classList.add('show');
+            try {
+                obj.classList.remove('hide');
+            } catch(err) {
+
+            }
+        }
+    } else {
+        for (let obj of thingsToChange) {
+            obj.classList.add('hide');
+            try {
+                obj.classList.remove('show');
+                setTimeout(function() {obj.style.filter = "opacity(0)";},900);
+            } catch(err) {
+                
+            }
+        }
+    }
+}
+
 document.getElementById('playbtn').addEventListener('click', function() {
 
     if (currentState == 'play') {
         document.getElementById('pa').classList.remove('out')
         document.getElementById('pl').classList.add('out')
+
+        // function to change the countdown timer's appearance
+        countdownChange('hide')
         currentState = 'pause';
     } else {
         document.getElementById('pa').classList.add('out')
         document.getElementById('pl').classList.remove('out')
+
+        // function to change the countdown timer's appearance
+        countdownChange('show')
         currentState = 'play'
     }
 })
